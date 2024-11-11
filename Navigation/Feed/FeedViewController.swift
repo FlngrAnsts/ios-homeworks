7143d0ef4999930712a6b4e00b9cbc299d4cf390
Anastasiya Felinger <felinger.nastya@gmail.com>
2024-11-11 19:37:28 +0300

    map

--- Navigation/Feed/FeedViewController.swift
+++ Navigation/Feed/FeedViewController.swift
@@ -13,6 +13,18 @@ class FeedViewController: UIViewController {
     let feedModel = FeedModel()
     var coordinator: FeedCoordinator?
     
+    private lazy var mapButton: CustomButton = {
+        
+        let button = CustomButton(title: "Карта", titleColor: .systemBlue){
+            self.mapButtonPressed()
+        }
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        return button
+        
+    }()
+    
     private lazy var actionButton: CustomButton = {
         
         let button = CustomButton(title: "Перейти", titleColor: .systemBlue){
@@ -85,6 +97,7 @@ class FeedViewController: UIViewController {
         view.backgroundColor = .systemGray4
         
         view.addSubview(stackView)
+        view.addSubview(mapButton)
         setupContraints()
         
     }
@@ -92,6 +105,10 @@ class FeedViewController: UIViewController {
     func setupContraints() {
         let safeAreaLayoutGuide = view.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
+            
+            mapButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 24),
+            mapButton.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+            
             stackView.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor),
         ])
@@ -105,4 +122,11 @@ class FeedViewController: UIViewController {
         
         postViewController.titlePost = Post.make()[0].author
     }
+    
+    func mapButtonPressed() {
+       let mapViewController = MapViewController()
+       
+       self.navigationController?.pushViewController(mapViewController, animated: true)
+       
+   }
 }
---
+++ Navigation/UIView/Extension.swift
@@ -0,0 +1,44 @@
+//
+//  Extension.swift
+//  Navigation
+//
+//  Created by Anastasiya on 27.09.2024.
+//
+
+import UIKit
+
+public extension UIImage {
+    func image(alpha: CGFloat) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        draw(at: .zero, blendMode: .normal, alpha: alpha)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    func rotate(radians: Float) -> UIImage? {
+        var newSize = CGRect(origin: CGPoint.zero, size: self.size).applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
+        // Trim off the extremely small float value to prevent core graphics from rounding it up
+        newSize.width = floor(newSize.width)
+        newSize.height = floor(newSize.height)
+        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        // Move origin to middle
+        context.translateBy(x: newSize.width/2, y: newSize.height/2)
+        // Rotate around middle
+        context.rotate(by: CGFloat(radians))
+        // Draw the image at its center
+        self.draw(in: CGRect(x: -self.size.width/2, y: -self.size.height/2, width: self.size.width, height: self.size.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    func imageWith(newSize: CGSize) -> UIImage {
+        let image = UIGraphicsImageRenderer(size: newSize).image { _ in
+            draw(in: CGRect(origin: .zero, size: newSize))
+        }
+        return image.withRenderingMode(renderingMode)
+    }
+    
+}
